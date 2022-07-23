01280fd61e962d3a7b1193968cb04234b16f3d8d
Marian Bucatariu <33843056+bmarian@users.noreply.github.com>
2022-07-23 11:36:48 +0300

    WIP server

--- snaxopedia-api/index.ts
+++ snaxopedia-api/index.ts
@@ -1,14 +1,21 @@
-import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.149.0/http/server.ts";
 import { routeHandler as snaxopediaRouteHandler } from "./snaxopedia.ts";
+import { imageRouteHandler as bugImageRouteHandler } from "./bugs.ts";
 
 const SNAXOPEDIA_ROUTE = new URLPattern({ pathname: "/snaxopedia" });
+const BUG_IMAGE_ROUTE = new URLPattern({ pathname: "/bugs/:fileName" });
 
-const handler = (req: Request): Response => {
+const handler = async (req: Request): Promise<Response> => {
   const { url } = req;
-  if (SNAXOPEDIA_ROUTE.exec(url)) return snaxopediaRouteHandler();
+
+  const snaxopediaMatch = SNAXOPEDIA_ROUTE.exec(url);
+  if (snaxopediaMatch) return snaxopediaRouteHandler();
+
+  const bugImageMatch = BUG_IMAGE_ROUTE.exec(url);
+  if (bugImageMatch) return await bugImageRouteHandler(req, bugImageMatch);
 
   return new Response("Route not found", { status: 404 });
 };
 
 console.log("Listening on http://localhost:8000")
-serve(handler);
+serve(handler, { port: 8000 });
--- snaxopedia-api/snaxopedia.ts
+++ snaxopedia-api/snaxopedia.ts
@@ -1,5 +1,12 @@
 import snaxopedia from "./assets/snaxopedia.json" assert { type: "json" };
 
 export const routeHandler = (): Response => {
-  return new Response(JSON.stringify(snaxopedia));
+  try {
+    const stringifySnaxopedia = JSON.stringify(snaxopedia)
+    console.log("Snaxopedia was successfully delivered!");
+    return new Response(stringifySnaxopedia, { status: 200 });
+  } catch (error) {
+    console.log("Error while parsing snaxopedia: ", error);
+    return new Response(error, { status: 400 });
+  }
 };
