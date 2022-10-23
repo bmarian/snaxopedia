0669784e32d03a826a78a07e971cc11041f13523
bmarian <33843056+bmarian@users.noreply.github.com>
2022-10-23 15:29:46 +0300

    Skeleton

--- snaxopedia-js/src/main.ts
+++ snaxopedia-js/src/main.ts
@@ -1,1 +1,100 @@
 import './style.css'
+
+type Bug = {
+  name: string,
+  location: string,
+  bug: string,
+  snack: string,
+  image: string,
+  calories?: string,
+  strategy?: string,
+  attributes?: string[],
+  isSelected?: boolean,
+  hasBeenSeen?: boolean,
+  hasBeenPhotographed?: boolean,
+};
+type SnaxopediaGroup = {
+  location: string,
+  bugs: Bug[],
+};
+
+let snaxopedia: Bug[] = []
+let groupedSnaxopedia: SnaxopediaGroup[] = []
+
+const changeSnaxopedia = (newSnaxopedia: any) => {
+  const getLocations = (): string[] => {
+    const listOfLocations = newSnaxopedia.map((bug: Bug) => bug.location)
+    return [...new Set(listOfLocations.sort((a: string, b: string) => a.localeCompare(b)))] as string[]
+  }
+
+  const groupBugsByLocation = (): {location: string, bugs: Bug[]}[] => {
+    const locations: string[] = getLocations()
+    return locations.reduce((acc: { location: any, bugs: any[] }[], location: string) => {
+      const bugsForLocation = newSnaxopedia
+        .filter((bug: Bug) => bug.location === location)
+        .sort((a: Bug, b: Bug) => a.name.localeCompare(b.name))
+      return [...acc, { location, bugs: bugsForLocation }]
+    }, [])
+  }
+
+  snaxopedia = newSnaxopedia
+  groupedSnaxopedia = groupBugsByLocation()
+}
+
+const loadSnaxopedia = async () => {
+  try {
+    const response = await fetch("http://localhost:8000/snaxopedia")
+    const newSnaxopedia = await response.json()
+    changeSnaxopedia(newSnaxopedia)
+  } catch (err) { console.log(err) }
+}
+
+const saveBugData = async (bug: Bug) => {
+  const { name } = bug
+  try {
+    await fetch(`http://localhost:8000/snaxopedia/${name}`, {
+      method: "POST",
+      mode: 'cors',
+      body: JSON.stringify(bug)
+    })
+  } catch (err) { console.log(err) }
+}
+
+const saveSelectedData = async (bug: Bug) => {
+  const { name } = bug
+  try {
+    await fetch(`http://localhost:8000/snaxopedia/selected/${name}`)
+  } catch (err) { console.log(err) }
+}
+
+const getSelectedBug = () => {
+  return snaxopedia.find((snack) => snack.isSelected)
+}
+
+const setSelectedBug = (bug: Bug) => {
+  const newSnaxopedia = snaxopedia.map((snack: Bug) => ({ ...snack, isSelected: snack.name === bug.name }))
+  changeSnaxopedia(newSnaxopedia)
+
+  saveSelectedData(bug)
+}
+
+const modifyBug = (bug: Bug, data: {} = {}) => {
+  const { name } = bug
+  const newSnaxopedia = snaxopedia.map((snack: Bug) => {
+    if (snack.name !== name) return { ...snack }
+
+    const modifiedSnack = { ...snack, ...data }
+    saveBugData(modifiedSnack)
+    return modifiedSnack
+  })
+  changeSnaxopedia(newSnaxopedia)
+}
+
+
+const toggleLoading = (loading: boolean): void => {
+  const loader = document.querySelector('.loader-container')
+  if (!loader) return
+
+  if (loading) loader.classList.remove("hidden")
+  else loader.classList.add("hidden")
+}
--- snaxopedia-react/src/App.tsx
+++ snaxopedia-react/src/App.tsx
@@ -10,60 +10,60 @@ export default function App() {
 
   const loadSnaxopedia = useCallback(async () => {
     try {
-      const response = await fetch("http://localhost:8000/snaxopedia");
-      const jsonSnaxopedia = await response.json();
+      const response = await fetch("http://localhost:8000/snaxopedia")
+      const jsonSnaxopedia = await response.json()
       setSnaxopedia(jsonSnaxopedia)
-    } catch (err) { console.log(err); }
+    } catch (err) { console.log(err) }
   }, [])
   const saveBugData = useCallback(async (bug: Bug) => {
-    const { name } = bug;
+    const { name } = bug
     try {
       await fetch(`http://localhost:8000/snaxopedia/${name}`, {
         method: "POST",
         mode: 'cors',
         body: JSON.stringify(bug)
-      });
-    } catch (err) { console.log(err); }
+      })
+    } catch (err) { console.log(err) }
   }, [])
   const saveSelectedData = useCallback(async (bug: Bug) => {
-    const { name } = bug;
+    const { name } = bug
     try {
-      await fetch(`http://localhost:8000/snaxopedia/selected/${name}`);
-    } catch (err) { console.log(err); }
+      await fetch(`http://localhost:8000/snaxopedia/selected/${name}`)
+    } catch (err) { console.log(err) }
   }, [])
 
   const getLocations = useCallback(() => {
-    const listOfLocations = snaxopedia.map((bug) => bug.location);
-    return [...new Set(listOfLocations.sort((a, b) => a.localeCompare(b)))];
+    const listOfLocations = snaxopedia.map((bug) => bug.location)
+    return [...new Set(listOfLocations.sort((a, b) => a.localeCompare(b)))]
   }, [snaxopedia])
   const getBugsGroupedByLocation = useCallback(() => {
-    const locations: string[] = getLocations();
+    const locations: string[] = getLocations()
     return locations.reduce((acc: { location: any, bugs: any[] }[], location: string) => {
       const bugsForLocation = snaxopedia
         .filter((bug) => bug.location === location)
-        .sort((a, b) => a.name.localeCompare(b.name));
-      return [...acc, { location, bugs: bugsForLocation }];
+        .sort((a, b) => a.name.localeCompare(b.name))
+      return [...acc, { location, bugs: bugsForLocation }]
     }, [])
   }, [snaxopedia])
   const getSelectedBug = useCallback(() => {
-    return snaxopedia.find((snack) => snack.isSelected);
+    return snaxopedia.find((snack) => snack.isSelected)
   }, [snaxopedia])
   const modifyBug = useCallback((bug: Bug, data: {} = {}) => {
-    const { name } = bug;
+    const { name } = bug
     const newSnaxopedia = snaxopedia.map((snack: Bug) => {
-      if (snack.name !== name) return { ...snack };
+      if (snack.name !== name) return { ...snack }
 
-      const modifiedSnack = { ...snack, ...data };
-      saveBugData(modifiedSnack);
-      return modifiedSnack;
-    });
-    setSnaxopedia(newSnaxopedia);
+      const modifiedSnack = { ...snack, ...data }
+      saveBugData(modifiedSnack)
+      return modifiedSnack
+    })
+    setSnaxopedia(newSnaxopedia)
   }, [snaxopedia])
   const setSelectedBug = useCallback((bug: Bug) => {
-    const newSnaxopedia = snaxopedia.map((snack: Bug) => ({ ...snack, isSelected: snack.name === bug.name }));
+    const newSnaxopedia = snaxopedia.map((snack: Bug) => ({ ...snack, isSelected: snack.name === bug.name }))
     setSnaxopedia(newSnaxopedia)
 
-    saveSelectedData(bug);
+    saveSelectedData(bug)
   }, [snaxopedia])
 
   useEffect(() => { loadSnaxopedia() }, [])
